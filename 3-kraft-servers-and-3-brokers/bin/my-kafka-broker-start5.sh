09dc6df6abfc073009762e285ee52df603c3e99f
Edoardo Comar <ecomar@uk.ibm.com>
2024-11-13 14:26:58 +0000

    xmx2g

--- 3-kraft-servers-and-3-brokers/bin/my-kafka-broker-start5.sh
+++ 3-kraft-servers-and-3-brokers/bin/my-kafka-broker-start5.sh
@@ -21,7 +21,7 @@ if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
 fi
 
 if [ "x$KAFKA_HEAP_OPTS" = "x" ]; then
-    export KAFKA_HEAP_OPTS="-Xmx1G -Xms1G"
+    export KAFKA_HEAP_OPTS="-Xmx2G -Xms2G"
 fi
 
 EXTRA_ARGS=${EXTRA_ARGS-'-name kafkaServer -loggc'}
